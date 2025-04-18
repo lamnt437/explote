862206575cecb813ee9a21ab2d3dcb437c4a8efc
Lam Nguyen <lamnt437@gmail.com>
2025-04-18 17:53:16 +0700

    allow entering the tasklist name to get details

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ def get_list_of_task_lists(takeout):
         print("key", key)
     task_lists = takeout['items']
     title_list = []
+    print("List of task list names:")
     for task_list in task_lists:
         title = task_list['title']
-        print("list_title", title)
+        print(title)
         title_list.append(title)
     return title_list
 
@@ -35,10 +36,11 @@ def get_tasks_by_task_list_name(takeout, title_to_get):
     return []
 
 task_list_titles = get_list_of_task_lists(takeout)
-print("task_list_titles", task_list_titles)
 
-task_to_get = get_tasks_by_task_list_name(takeout, 'Pending 0')
-print("task_to_get", task_to_get)
+task_list_to_get = input("Enter tasklist name to get: ")
+
+task_to_get = get_tasks_by_task_list_name(takeout, task_list_to_get)
+# print("task_to_get", task_to_get)
 
 for task in task_to_get:
     print("task: ", task.get('title'))
